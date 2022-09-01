29a6c2ae2c47e12e877a5c81815ad2718f9e4034
CandidoNycolas <nycolas.csilva@gmail.com>
2022-09-01 19:38:19 -0300

    ajuste na aula 16

--- src/dom/nycolas/cursojava/labs/aulas14_15/Exercicio16.java
+++ src/dom/nycolas/cursojava/labs/aulas14_15/Exercicio16.java
@@ -10,12 +10,22 @@ public class Exercicio16 {
 		
 		System.out.println("Informe o valor de a");
 		double a = scan.nextDouble();
+		if (a == 0) {
+			System.out.println("Esta não é uma equação do segundo grau");
+			return ; //System.exit(0);
+		}
 		System.out.println("Informe o valor de b");
 		double b = scan.nextDouble();
 		System.out.println("Informe o valor de c");
 		double c = scan.nextDouble();
 			
 		double delta = ((Math.pow (b, 2)) - (4*a*c));
+		
+		if(delta < 0) {
+			System.out.println("delta negativo, favor verificar os valores");
+			return ;
+		}
+		
 		double x1 = (-b + (Math.sqrt(delta))/2*a); 
 		double x2 = (-b - (Math.sqrt(delta))/2*a);
 		
@@ -23,14 +33,7 @@ public class Exercicio16 {
 		if (delta == 0) {
 			System.out.println("Esta equação possui apenas uma raíz real. A raíz é: " + x1);
 		} else if (delta > 0) {
-			System.out.println("As raízes desta equação são: " + x1 + " e " + x2);
-		} else if (a == 0) {
-			System.out.println("Esta não é uma equação do segundo grau");
-			System.exit(0);
-		} else if (delta == 0) {
-			System.out.println("Esta equação não possui raízes reais");
-			System.exit(0);
-			}
+			System.out.println("As raizes desta equacao sao: " + x1 + " e " + x2);
 		}
-
+	}
 }
